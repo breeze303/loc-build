537a6a04b5c5e450ff0c26e6765c9cce7ae6e133
breeze303 <122870904+breeze303@users.noreply.github.com>
2026-02-07 15:52:04 +0800

    Fix luci-app-daed and update SmartDNS Makefile
    Fix issues related to luci-app-daed and update SmartDNS Makefile hash.

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -102,4 +102,15 @@ fi
 DAED_MAKEFILE=$(find . -maxdepth 4 -name Makefile | xargs grep -l "PKG_NAME:=daed")
 if [ -n "$DAED_MAKEFILE" ]; then
     sed -i 's/GOEXPERIMENT=/# &/' "$DAED_MAKEFILE"
+	echo "已修复daed。"
+fi
+
+#临时修复smartdns哈希问题
+FILE="./smartdns/Makefile"
+
+if [ -f "$FILE" ]; then
+    sed -i 's/609fec024396a3a26278ef9fe7bd49aeca478e3163fc53c699a5f402fa0320f0/f8bfb91ae0992dd62392ebb2b7d968d514f7cbc3cc6a5d975dafdd6b27bf0a0c/g' "$FILE"
+    echo "SmartDNS Makefile 已更新。"
+else
+    echo "跳过：未找到 SmartDNS Makefile。"
 fi
