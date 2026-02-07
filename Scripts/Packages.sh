b087008ce606daa10072f814f761e7cd7879ca06
breeze303 <122870904+breeze303@users.noreply.github.com>
2026-02-07 11:42:54 +0800

    Add smartdns and lucky packages to Packages.sh
    Added new packages for smartdns and lucky.

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -57,7 +57,10 @@ UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
 
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+UPDATE_PACKAGE "luci-app-smartdns" "pymumu/luci-app-smartdns" "master"
+UPDATE_PACKAGE "smartdns" "pymumu/openwrt-smartdns" "master" "smartdns"
+
+UPDATE_PACKAGE "lucky" "sirpdboy/luci-app-lucky" "main"
 
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
